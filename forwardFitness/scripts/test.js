4fcdb999510d6eb3693ca0981d5adfaba6866603
dmwellsSDBD <55493178+dmwellsSDBD@users.noreply.github.com>
2023-02-03 17:51:08 +0000

    uplpaded Forward Fitness

---
+++ forwardFitness/scripts/test.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function day() {
+    var day = document.getElementById("date");
+    if (day === "Saturday") {
+        document.write("I love Saturdays");
+    } else {
+        document.write("I wish it was Saturday");
+    }
+}
